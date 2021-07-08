d39f3ab679b495b9200dec22dd17769e249cf36d
Ethan Ortega <ethan_ortega@ciencias.unam.mx>
2021-07-08 17:22:43 -0500

    Emacs config and script added

---
+++ dot_config/chemacs/profiles.el
@@ -0,0 +1,3 @@
+(("default" . ((user-emacs-directory . "~/.config/emacs-default")))
+ ("doom" . ((user-emacs-directory . "~/.config/doom-emacs")))
+ ("space" . ((user-emacs-directory . "~/.config/spacemacs"))))
